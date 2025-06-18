0b0e72579cfd5b2493445e394cf63fed1c91b118
prantinin <francine_santos@dlsu.edu.ph>
2025-06-18 13:45:19 +0800

    Titles

--- JS/general.js
+++ JS/general.js
@@ -1,7 +1,21 @@
 function openSidebar() {
     document.querySelector(".sidebar").classList.add("active");
+    document.addEventListener("click", handleOutsideClick);
 }
 
 function closeSidebar() {
     document.querySelector(".sidebar").classList.remove("active");
+    document.removeEventListener("click", handleOutsideClick);
+}
+
+function handleOutsideClick(event) {
+    const sidebar = document.querySelector(".sidebar");
+    const openButton = document.querySelector(".open-menu");
+
+    if (
+        !sidebar.contains(event.target) &&
+        !openButton.contains(event.target)
+    ) {
+        closeSidebar();
+    }
 }
