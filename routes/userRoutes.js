ef638a155a7ec81341f2d4cbc99ec8c495f38b21
Eclair513 <82280762+Eclair513@users.noreply.github.com>
2025-07-14 22:45:10 +0800

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ router.get('/viewprofile/:idNum', userController.getViewProfileStudent);
 router.get('/profile/:email', userController.getViewProfileTech);
 
 router.get('/editprofile/:idNum', userController.getEditProfile);
-router.get('/editprofile/:idNum', userController.postEditProfile);
+router.post('/editprofile/:idNum', userController.uploadProfilePicture, userController.postEditProfile);
 
 router.get('/searchusers', userController.getSearchUsers);
 router.get('/searchusers', userController.postSearchUsers);
