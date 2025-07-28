62752d2a07824a20cb430e6fede70f5f29d383be
sopherstar <sophia_ilustre@dlsu.edu.ph>
2025-07-28 12:44:47 +0800

    hid edit button for expired reservations

---
+++ public/JS/Tviewreservs.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const resDate = document.getElementById("resDate");
+  const confirmBtn = document.querySelector(".confirmRes");
+
+  const today = new Date();
+  const reservationDate = new Date(resDate.value);
+
+  const notExpired = reservationDate.setHours(0,0,0,0) >= today.setHours(0,0,0,0);
+
+  // Continue original setup
+  document.querySelector("#resDate").addEventListener("change", allowFields);
+  document.querySelector(".timeSlot").addEventListener("change", allowFields);
+  document.querySelector(".labSelect").addEventListener("change", allowFields);
+  populateDateAndTime();
+
+  const reservSuccess = document.getElementById("reservSuccess");
+  if (reservSuccess) {
+    setTimeout(() => {
+      reservSuccess.remove();
+    }, 3000);
+  }
+});
